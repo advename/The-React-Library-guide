f8bdf81e54112b09ce32987127ad93475aa92445
Lars <lal@userneeds.com>
2022-01-06 14:43:45 +0100

    temp

---
+++ my-app/.yalc/my-lib/src/index.ts
@@ -0,0 +1,3 @@
+// import "./scss/main.scss"
+
+export * from "./components";
---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+// @ts-nocheck 
+import React from "react";
+import { Button } from "my-lib";
+
+function App() {
+  return (
+    <div>
+      <h1>Some page</h1>
+      <Button text="hi"></Button>
+    </div>
+  );
+}
+
+export default App;
